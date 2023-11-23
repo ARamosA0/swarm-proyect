d992e17668076df76c45902beb1756fea5a148b0
ARamosA0 <aldoramosaro@gmail.com>
2023-11-23 20:53:59 +0000

    mejoras

--- vote/app.py
+++ vote/app.py
@@ -7,6 +7,7 @@ import json
 import logging
 import math
 import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
@@ -34,9 +35,9 @@ def get_data():
     return consolidated_df
 
 def cosine_similarity_manual(vec1, vec2):
-    dot_product = sum(a * b for a, b in zip(vec1, vec2))
-    magnitude_vec1 = math.sqrt(sum(a**2 for a in vec1))
-    magnitude_vec2 = math.sqrt(sum(b**2 for b in vec2))
+    dot_product = np.dot(vec1, vec2)
+    magnitude_vec1 = np.linalg.norm(vec1)
+    magnitude_vec2 = np.linalg.norm(vec2)
     
     if magnitude_vec1 == 0 or magnitude_vec2 == 0:
         return 0  # Evitar la división por cero
@@ -44,18 +45,79 @@ def cosine_similarity_manual(vec1, vec2):
     return dot_product / (magnitude_vec1 * magnitude_vec2)
 
 def find_nearest_neighbors_manual(user_id, ratings_df, num_neighbors=10):
-    user_ratings = ratings_df[ratings_df['userId'] == user_id].drop(columns=['userId', 'timestamp']).values[0]
-    other_users_ratings = ratings_df[ratings_df['userId'] != user_id].drop(columns=['userId', 'timestamp'])
+    user_ratings = ratings_df.loc[ratings_df['userId'] == user_id].drop(columns=['userId', 'timestamp']).values[0]
+    other_users_ratings = ratings_df.loc[ratings_df['userId'] != user_id].drop(columns=['userId', 'timestamp']).values
 
-    similarities = [
-        cosine_similarity_manual(user_ratings, row.values)
-        for _, row in other_users_ratings.iterrows()
-    ]
+    similarities = np.array([
+        cosine_similarity_manual(user_ratings, row)
+        for row in other_users_ratings
+    ])
 
-    nearest_neighbors_indices = sorted(range(len(similarities)), key=lambda i: similarities[i], reverse=True)[:num_neighbors]
+    nearest_neighbors_indices = np.argsort(similarities)[::-1][:num_neighbors]
     nearest_neighbors_ids = ratings_df.iloc[nearest_neighbors_indices]['userId'].tolist()
 
     return nearest_neighbors_ids
+
+def manhattan(rating1, rating2):
+    distance = 0
+    commonRatings = False
+
+    for key in rating1:
+        if key in rating2:
+            if not math.isnan(rating1[key]) and not math.isnan(rating2[key]):
+                distance += abs(rating1[key] - rating2[key])
+                commonRatings = True
+            else:
+                pass
+
+    if commonRatings:
+        return distance
+    else:
+        return -1
+
+def computeNearestNeighbor(username, users):
+    """creates a sorted list of users based on their distance to username"""
+    distances = []
+    for user in users:
+        #print(user)
+        if user != username:
+
+          #Cambiar por la distancia a usar
+            #distance = similitud_coseno_nan(users[user], users[username])
+            #distance = correlacion_pearson_nan(users[user], users[username])
+            distance = manhattan(users[user], users[username])
+            distances.append((distance, user))
+    # sort based on distance -- closest first
+    distances.sort()
+    return distances
+
+def recommend(username, users):
+    """Give list of recommendations"""
+    # first find all nearest neighbors
+    nearest_neighbors = computeNearestNeighbor(username, users)
+    #print(nearest_neighbors)
+
+    recommendations = []
+
+    # now find bands neighbors rated that user didn't
+    for nearest_neighbor in nearest_neighbors[:5]:
+        print(nearest_neighbor)
+        neighbor_ratings = users[nearest_neighbor[1]]
+        user_ratings = users[username]
+        #print(user_ratings)
+
+        neighbor_ratings = {artist: rating for artist, rating in neighbor_ratings.items() if not math.isnan(rating)}
+        user_ratings = {artist: rating for artist, rating in user_ratings.items() if not math.isnan(rating)}
+
+        #print(neighbor_ratings)
+        #print(user_ratings)
+        for artist in neighbor_ratings:
+            if artist not in user_ratings:
+                recommendations.append((artist, neighbor_ratings[artist]))
+                #print(neighbor_ratings[artist])
+
+    # using the fn sorted for variety - sort is more efficient
+    return sorted(recommendations, key=lambda artist_tuple: artist_tuple[1], reverse=True)
 
 @app.route("/", methods=['POST', 'GET'])
 def hello():
@@ -68,11 +130,13 @@ def hello():
         
         if 'calculate' in request.form:
             user_id = int(request.form.get('user_id'))
+            #user_name = request.form.get('user_id')
             
-            cosine_neighbors = find_nearest_neighbors_manual(user_id, ratings)
-            
+            cn = find_nearest_neighbors_manual(user_id, ratings)
+            #nn = computeNearestNeighbor(user_id, ratings)
+            #r = recommend(user_name, ratings)
             # Convertir la lista a formato JSON antes de almacenar en Redis
-            neighbors_data = json.dumps({'user_id': user_id, 'neighbors': cosine_neighbors})
+            neighbors_data = json.dumps({'user_id': user_id, 'neighbors': cn})
             redis.rpush('cosine_neighbors', neighbors_data)
             app.logger.info(neighbors_data)
             if redis.exists('cosine_neighbors'):
